92c01528051157afad4d11782e6005e8b5a8b77c
Alexander Novikov <mitskifys@gmail.com>
2021-12-12 14:08:08 +0300

    initial commit

---
+++ src/task_one/core.clj
@@ -0,0 +1,17 @@
+(ns task-one.core
+  (:gen-class))
+
+(require '[clojure.repl :refer :all])
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(comment
+  (defn parse_string 
+    [string count] 
+    (
+     print string
+    ))
+)
